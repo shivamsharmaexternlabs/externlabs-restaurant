21020686b04b5ce88bd938b4d80f48d50f01b13f
shivam <shivamsharma@externlabs.com>
2023-11-06 18:00:42 +0530

    gvdj

--- src/Authantication/Login/Login.js
+++ src/Authantication/Login/Login.js
@@ -19,7 +19,7 @@ const Login = () => {
   useEffect(() => {
     if (User?.data[0]?.status === 200) {
       setLoadSpiner(false);
-      navigate("/:id/menu");
+      navigate("/dashboard");
       // window.location.reload(true);
     }
 
