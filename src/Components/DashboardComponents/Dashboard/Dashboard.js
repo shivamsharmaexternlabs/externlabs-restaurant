b2678beef14a6f721002bde9ca89a5ec6d602146
shivam <shivamsharma@externlabs.com>
2023-11-07 17:53:45 +0530

    dashboard qr code done

--- src/Components/DashboardComponents/Dashboard/Dashboard.js
+++ src/Components/DashboardComponents/Dashboard/Dashboard.js
@@ -15,7 +15,9 @@ import item2 from '../../../images/item2.svg'
 import qrimg from '../../../images/qr.png'   
  
 import { useNavigate } from 'react-router-dom'
-import { reactLocalStorage } from 'reactjs-localstorage'  
+import { reactLocalStorage } from 'reactjs-localstorage'
+import item2 from '../../../images/item2.svg'
+import qrimg from '../../../images/qr.png'  
  
 
  const Dashboard = () => {
