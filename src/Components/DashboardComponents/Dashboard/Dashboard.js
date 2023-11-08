d3dfd48857b184d0e52ff0a0c2c01697eeeba741
Om Veer Singh <omveersingh@externlabs.com>
2023-11-08 14:21:46 +0530

    Favorite Dishes API Done.

--- src/Components/DashboardComponents/Dashboard/Dashboard.js
+++ src/Components/DashboardComponents/Dashboard/Dashboard.js
@@ -1,64 +1,91 @@
-import React,{useEffect, useState} from 'react' 
-import { useDispatch,useSelector } from 'react-redux'
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import './dashboard.css'
 import { saveAs } from "file-saver";
-import DashboardSidebar from '../DashboardSidebar/DashboardSidebar' 
+import DashboardSidebar from '../DashboardSidebar/DashboardSidebar'
 import DashboardLayout from '../DashboardLayout/DashboardLayout'
-import {ManagerSlice} from "../../../Redux/slices/managerSlice"
-import {GetQrCodeSlice} from "../../../Redux/slices/qrCodeSlice"
+import { ManagerSlice } from "../../../Redux/slices/managerSlice"
+import { GetQrCodeSlice } from "../../../Redux/slices/qrCodeSlice"
 import arrow2 from '../../../images/arrow2.svg'
 import dish1 from '../../../images/dish1.png'
 import dish2 from '../../../images/dish2.png'
 import dish3 from '../../../images/dish3.png'
 import user from '../../../images/user.png'
 import item2 from '../../../images/item2.svg'
-import qrimg from '../../../images/qr.png'   
+import qrimg from '../../../images/qr.png'
 import { useNavigate } from 'react-router-dom'
-import { reactLocalStorage } from 'reactjs-localstorage'  
-   
- 
+import { reactLocalStorage } from 'reactjs-localstorage'
+import { favoriteMenuSlice } from '../../../Redux/slices/menuSlice'
 
- const Dashboard = () => {
-   const dispatch=useDispatch();
-   const navigate=useNavigate();
-   const [data,setData]=useState({results:[]});
-   const [QrImage,setQrImage]=useState("")
-   const ManagerApiSelectorData = useSelector((state) => state.ManagerApiData?.data);
-   const QrApiSelectorData=useSelector((state) => state.QrCodeApiData?.data)
-   let RestaurantIdLocalData = reactLocalStorage.get("RestaurantId", false); 
-   console.log("dfsagh",QrImage)
-   let BearerToken = reactLocalStorage.get("Token", false);
 
- 
-   useEffect(() => {
+
+const Dashboard = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [data, setData] = useState({ results: [] });
+  const [QrImage, setQrImage] = useState("")
+  const [ActiveFavoriteCategory, setActiveFavoriteCategory] = useState({});
+
+  const ManagerApiSelectorData = useSelector((state) => state.ManagerApiData?.data);
+  const QrApiSelectorData = useSelector((state) => state.QrCodeApiData?.data)
+  const MenuApiSelectorData = useSelector((state) => state.MenuApiData);
+
+  let RestaurantIdLocalData = reactLocalStorage.get("RestaurantId", false);
+
+  console.log("dfsagh", QrImage)
+  let BearerToken = reactLocalStorage.get("Token", false);
+
+  console.log("MenuApiSelectorDatauuuuuuuu", MenuApiSelectorData)
+  console.log("ActiveFavoriteCategory", ActiveFavoriteCategory)
+
+  useEffect(() => {
     setData(ManagerApiSelectorData?.data)
     setQrImage(QrApiSelectorData?.data?.results[0]?.qrcode)
-}, [ManagerApiSelectorData,QrApiSelectorData]);
+  }, [ManagerApiSelectorData, QrApiSelectorData]);
 
-useEffect(() => {
+  useEffect(() => {
 
-  let ManagerSlicePayload={
-      Token:BearerToken,
-      pageination:1
+    let ManagerSlicePayload = {
+      Token: BearerToken,
+      pageination: 1
+    }
+    dispatch(ManagerSlice(ManagerSlicePayload))
+
+  }, [BearerToken])
+
+
+  const QrCodeDownloadFun = () => {
+    let url = QrImage
+    saveAs(url, "Twitter-logo");
   }
-  dispatch(ManagerSlice(ManagerSlicePayload))
-    
-}, [BearerToken])
+  
+  useEffect(() => {
 
-const QrCodeDownloadFun = () => { 
-  let url = QrImage
-  saveAs(url, "Twitter-logo");
-}
-useEffect(()=>{
-  let resturantid=RestaurantIdLocalData
-  dispatch(GetQrCodeSlice(resturantid)) 
-},[])    
- 
+    let resturantid = RestaurantIdLocalData
+    dispatch(GetQrCodeSlice(resturantid))
+    dispatch(favoriteMenuSlice({ "RestaurantId": RestaurantIdLocalData }));
+
+    // setActiveFavoriteCategory(MenuApiSelectorData?.favoriteMenuSliceReducerData?.data?.[0]?.menu_id)
+  }, [])
+
+
+  useEffect(() => {
+
+    setActiveFavoriteCategory(MenuApiSelectorData?.favoriteMenuSliceReducerData?.data?.[0])
+  }, [MenuApiSelectorData?.favoriteMenuSliceReducerData])
+
+
+  const FavoriteCategoryTabFun = (e, categoryItem) => {
+    setActiveFavoriteCategory(categoryItem);
+  }
+
+  console.log("bvsdsdds",ActiveFavoriteCategory)
+
   return (
     <>
       <DashboardLayout  >
         <div className='dasboardbody'>
-          <DashboardSidebar   />
+          <DashboardSidebar />
           <div className='contentpart dashboardpage'>
             <div className='bannerbox'>
               <h2> Good Morning</h2>
@@ -75,7 +102,28 @@ useEffect(()=>{
                   <div className='topdishestabpart'>
                     <nav>
                       <div class="nav nav-tabs" id="nav-tab" role="tablist">
-                        <button class="nav-link active" id="nav-dishes1-tab" data-bs-toggle="tab" data-bs-target="#nav-dishes1" type="button" role="tab" aria-controls="nav-dishes1" aria-selected="true">
+                        {/* FAVORITE DISHES MANAGEMENT */}
+                        {
+                          MenuApiSelectorData?.favoriteMenuSliceReducerData?.data?.map((items, favoriteId) => {
+                            console.log("hdvfbjsfhvhsvh", items)
+                            return <button key={favoriteId} onClick={(e) => FavoriteCategoryTabFun(e, items)} class={`nav-link ${items?.menu_id==ActiveFavoriteCategory?.menu_id?"active":""}`} id="nav-dishes1-tab" data-bs-toggle="tab" data-bs-target="#nav-dishes1" type="button" role="tab" aria-controls="nav-dishes1" aria-selected="true">
+                              <div>
+                                <figure>
+                                  <img src={dish1} alt="img" />
+                                </figure>
+                                <h3>{items?.category}</h3>
+                                <div className='buttonbox'>
+                                  <svg xmlns="http://www.w3.org/2000/svg" width="5" height="7" viewBox="0 0 5 7" fill="none">
+                                    <path d="M0.915527 1.23392L3.48241 3.8008L0.915527 6.36768" stroke-width="1.10009" stroke-linecap="round" stroke-linejoin="round" />
+                                  </svg>
+                                </div>
+                              </div>
+                            </button>
+
+                          })
+                        }
+
+                        {/* <button class="nav-link active" id="nav-dishes1-tab" data-bs-toggle="tab" data-bs-target="#nav-dishes1" type="button" role="tab" aria-controls="nav-dishes1" aria-selected="true">
                           <div>
                             <figure>
                               <img src={dish1} alt="img" />
@@ -101,23 +149,7 @@ useEffect(()=>{
                               </svg>
                             </div>
                           </div>
-                        </button>
-
-
-
-                        <button class="nav-link " id="nav-dishes3-tab" data-bs-toggle="tab" data-bs-target="#nav-dishes3" type="button" role="tab" aria-controls="nav-dishes3" aria-selected="true">
-                          <div>
-                            <figure>
-                              <img src={dish2} alt="img" />
-                            </figure>
-                            <h3>Pizza</h3>
-                            <div className='buttonbox'>
-                              <svg xmlns="http://www.w3.org/2000/svg" width="5" height="7" viewBox="0 0 5 7" fill="none">
-                                <path d="M0.915527 1.23392L3.48241 3.8008L0.915527 6.36768" stroke-width="1.10009" stroke-linecap="round" stroke-linejoin="round" />
-                              </svg>
-                            </div>
-                          </div>
-                        </button>
+                        </button> */}
 
                       </div>
                     </nav>
@@ -125,54 +157,38 @@ useEffect(()=>{
                     <div class="tab-content" id="nav-tabContent">
                       <div class="tab-pane fade show active" id="nav-dishes1" role="tabpanel" aria-labelledby="nav-dishes1-tab" tabindex="0">
                         <ul>
-                          <li>
-                            <h4>Spaghetti</h4>
+                        {
+                          ActiveFavoriteCategory?.item_id?.map((favoriteItem, favoriteDishesId) => {
+                            {/* console.log("favoriteItem", favoriteItem) */}
+                            return <li>
+                            <h4>{favoriteItem?.item_name}</h4>
                             <div className='tabinfo'>
                               <div className='leftpart'>
-                                <p>Lorem ipsum dolor sit amet consectetur.</p>
-                                <span className='price'>$7.29</span>
+                                <p>{favoriteItem?.description}</p>
+                                <span className='price'>{`$${favoriteItem?.item_price}`}</span>
                               </div>
                               <div className='rightpart'>
                                 <img src={dish3} alt='img' />
                               </div>
                             </div>
                           </li>
-                          <li>
+
+                          })
+                        }
+
+                          {/* <li>
                             <h4>Spaghetti</h4>
                             <div className='tabinfo'>
                               <div className='leftpart'>
                                 <p>Lorem ipsum dolor sit amet consectetur.</p>
-                                <span className='price'>$7.29</span>
-                              </div>
-                              <div className='rightpart'>
-                                {/*    <img src={dish3} alt='img' /> */}
-                              </div>
-                            </div>
-                          </li>
-                          <li>
-                            <h4>Spaghetti</h4>
-                            <div className='tabinfo'>
-                              <div className='leftpart'>
-                                <p>Lorem ipsum dolor sit amet consectetur.</p>
-                                <span className='price'>$7.29</span>
+                                <span className='price'>$7.287789</span>
                               </div>
                               <div className='rightpart'>
                                 <img src={dish3} alt='img' />
                               </div>
                             </div>
-                          </li>
-                          <li>
-                            <h4>Spaghetti</h4>
-                            <div className='tabinfo'>
-                              <div className='leftpart'>
-                                <p>Lorem ipsum dolor sit amet consectetur.</p>
-                                <span className='price'>$7.29</span>
-                              </div>
-                              <div className='rightpart'>
-                                {/* <img src={dish3} alt='img' /> */}
-                              </div>
-                            </div>
-                          </li>
+                          </li> */}
+
 
                         </ul>
                       </div>
@@ -234,7 +250,7 @@ useEffect(()=>{
                 <div className='managerstablepart'>
                   <div className='title'>
                     <h2>Managers</h2>
-                    <button type='button' onClick={(e)=>navigate("/manager")}> View All <img src={arrow2} alt='img' /> </button>
+                    <button type='button' onClick={(e) => navigate("/manager")}> View All <img src={arrow2} alt='img' /> </button>
                   </div>
                   <div class="managerstable">
                     <table className='table'>
@@ -246,12 +262,12 @@ useEffect(()=>{
                       </tr>
                       {data?.results?.map((items, id) => {
                         return <tr>
-                                 <td> <img src={user} alt='img' /> </td>
-                                 <td>{`${items?.first_name} ${items?.last_name} `}</td>
-                                 <td>{items?.email}</td>
-                                 <td>Lorem ipsum dolor sit amet consetur dign....</td>    
-                                </tr>
-                       })}
+                          <td> <img src={user} alt='img' /> </td>
+                          <td>{`${items?.first_name} ${items?.last_name} `}</td>
+                          <td>{items?.email}</td>
+                          <td>Lorem ipsum dolor sit amet consetur dign....</td>
+                        </tr>
+                      })}
 
                     </table>
                   </div>
@@ -278,11 +294,11 @@ useEffect(()=>{
 
                 </div>
                 <div className='qrbox'>
-                    <img src={QrImage} alt='img' />
-                    <div className='info'>
-                      <span>Date: 1/11/23</span>
-                      <button type='button'  onClick={(e) => QrCodeDownloadFun()}>Download </button>
-                    </div>
+                  <img src={QrImage} alt='img' />
+                  <div className='info'>
+                    <span>Date: 1/11/23</span>
+                    <button type='button' onClick={(e) => QrCodeDownloadFun()}>Download </button>
+                  </div>
                 </div>
 
               </div>
