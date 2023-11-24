d289238fc8675f4278b74caf1ca58a78f47ba7dc
Astha Sharma <aniruddh.a@neosoftmail.com>
2023-11-24 18:30:35 +0530

    indexing drag and drop

--- src/Components/DashboardComponents/DndCategories/DndCategories.js
+++ src/Components/DashboardComponents/DndCategories/DndCategories.js
@@ -9,79 +9,47 @@ function DndCategories() {
     const dispatch = useDispatch();
     const MenuApiSelectorData = useSelector((state) => state.MenuApiData);
     const [draggedItem, setDraggedItem] = useState(null);
-     const [items, setItems] = useState(MenuApiSelectorData?.GetMenuCategoryReducerData?.data);
-    // const [items, setItems] = useState([
-    //     {
-    //         "menu_id": "d17e8efb-7fa6-439b-a9d0-9ea2f7175cd0",
-    //         "category": "paneer Pizza",
-    //         "category_image": "https://restaurant-qr-staging.s3.ap-south-1.amazonaws.com/menu/download_GZuBy4B.png",
-    //         "index": 0
-    //     },
-    //     {
-    //         "menu_id": "d13a8cda-bc78-454d-815f-383773097c39",
-    //         "category": "burger",
-    //         "category_image": null,
-    //         "index": 1
-    //     },
-    //     {
-    //         "menu_id": "86bd1171-38c8-4311-99cd-7683c5903332",
-    //         "category": "Pizza",
-    //         "category_image": null,
-    //         "index": 2
-    //     },
-    //     {
-    //         "menu_id": "3f4dadfc-a085-48cd-864c-6edd92e47d1e",
-    //         "category": "chicken lolipop",
-    //         "category_image": null,
-    //         "index": 3
-    //     },
-    //     {
-    //         "menu_id": "0f533415-196b-4baa-9d3b-ce3c568a61c6",
-    //         "category": "Paneer",
-    //         "category_image": null,
-    //         "index": 4
-    //     },
-    //     {
-    //         "menu_id": "eaaf1d9a-dbbc-4dcd-903b-40922b179d61",
-    //         "category": "chicken ",
-    //         "category_image": null,
-    //         "index": 5
-    //     },
-    //     {
-    //         "menu_id": "a2579b7d-1672-44dc-9754-02172141f2e1",
-    //         "category": "fries",
-    //         "category_image": null,
-    //         "index": 6
-    //     },
-    //     {
-    //         "menu_id": "a2579b7d-1672-44dc-9754-02172141f2e1",
-    //         "category": "fries",
-    //         "category_image": null,
-    //         "index": 7
-    //     },
-    //     {
-    //         "menu_id": "a2579b7d-1672-44dc-9754-02172141f2e1",
-    //         "category": "fries",
-    //         "category_image": null,
-    //         "index": 8
-    //     }, {
-    //         "menu_id": "a2579b7d-1672-44dc-9754-02172141f2e1",
-    //         "category": "fries",
-    //         "category_image": null,
-    //         "index": 9
-    //     }, {
-    //         "menu_id": "a2579b7d-1672-44dc-9754-02172141f2e1",
-    //         "category": "fries",
-    //         "category_image": null,
-    //         "index": 10
-    //     },
-    //     {
-    //         "menu_id": "a2579b7d-1672-44dc-9754-02172141f2e1",
-    //         "category": "fries",
-    //         "category_image": null,
-    //         "index": 11
-    //     }
-    // ]);
+    // const [items, setItems] = useState(MenuApiSelectorData?.GetMenuCategoryReducerData?.data);
+    const [items, setItems] = useState([
+    {
+        "menu_id": "b8712be2-6dd8-47ed-aa96-f5c17ae88e0d",
+        "category": "cake",
+        "category_image": null,
+        "index": 1
+    },
+    {
+        "menu_id": "11300c2d-3c66-47af-a3cc-467bbcf00f90",
+        "category": "drink",
+        "category_image": "https://restaurant-qr-staging.s3.ap-south-1.amazonaws.com/menu/diskette_Yr6ex9S.png",
+        "index": 2
+    },
+    {
+        "menu_id": "d17e8efb-7fa6-439b-a9d0-9ea2f7175cd0",
+        "category": "paneer Pizza",
+        "category_image": null,
+        "index": 3
+    },
+    
+    {
+        "menu_id": "d13a8cda-bc78-454d-815f-383773097c39",
+        "category": "burger",
+        "category_image": null,
+        "index": 4
+    },
+    {
+        "menu_id": "86bd1171-38c8-4311-99cd-7683c5903332",
+        "category": "Pizza",
+        "category_image": null,
+        "index": 5
+    },
+    {
+        "menu_id": "0f533415-196b-4baa-9d3b-ce3c568a61c6",
+        "category": "Paneer",
+        "category_image": null,
+        "index": 6
+    },
+  
+]);
     const [ActiveCategory, setActiveCategory] = useState(undefined);
     console.log("items", items)
 
@@ -122,39 +90,50 @@ function DndCategories() {
 
     const handleDrop = (e, item) => {
         e.preventDefault();
-
+    
         if (!draggedItem || draggedItem === item) {
             return;
         }
-
+    
         // Find the index of the dragged and target items
         const draggedIndex = items.findIndex((el) => el.menu_id === draggedItem.menu_id);
         const targetIndex = items.findIndex((el) => el.menu_id === item.menu_id);
-
+    
         const updatedItems = [...items];
-        // Swap the positions of the dragged and target items
-        [updatedItems[draggedIndex], updatedItems[targetIndex]] = [updatedItems[targetIndex], updatedItems[draggedIndex]];
-
-        // Update the index field for all items
-        const finalItems = updatedItems.map((item, index) => ({ ...item, index }));
-
+        const newItems = [];
+    
+        // Iterate over the items and reindex them
+        updatedItems.forEach((originalItem, index) => {
+            console.log("originalItem",originalItem)
+            if (index === targetIndex) {
+                // Insert the dragged item at the target position
+                console.log("index1",index,targetIndex)
+                newItems.push({ ...draggedItem, index: index + 1 });
+            }
+            // Skip the dragged item if it's not at the target position
+            if (index !== draggedIndex) {
+                console.log("index2",index,draggedIndex)
+                const newIndex = index >= targetIndex ? index + 2 : index;
+                newItems.push({ ...originalItem, index: newIndex });
+            }
+        });
+    
         // Extract only menu_id and index properties for the payload
         const payload = {
-            data: finalItems.map(({ menu_id, index }) => ({ menu_id, index })),
+            data: newItems.map(({ menu_id, index }) => ({ menu_id, index })),
         };
-
-        setItems(finalItems);
-        console.log("newItems", finalItems);
+    
+        setItems(newItems);
+        console.log("newItems", newItems);
         console.log("payload", payload);
-
+    
         // Dispatch an action to update the Redux store with the new order
         dispatch(UpdateMenuCategoryAfterDragAndDrop(payload));
-
+    
         setDraggedItem(null);
     };
-
-
-
+    
+ 
     // const handleDrop = (e, item) => {
     //     e.preventDefault();
 
@@ -190,7 +169,7 @@ function DndCategories() {
     };
 
     return (
-        <div style={{ width: "100%", height: "100vh", display: "flex", flexDirection: "column",backgroundColor: "FBF6F2",padding:"30px" }}>
+        <div style={{ width: "100%", height: "100vh", display: "flex", flexDirection: "column", backgroundColor: "FBF6F2", padding: "30px" }}>
             <h1 className='recorded-cat-head'>Reorder Categories</h1>
             <div
             // className="categorycontent"
@@ -221,15 +200,18 @@ function DndCategories() {
                                     style={{ width: "18rem" }}
                                 > */}
                                 <div>
+                                    <div className='indx-div'>
+                                        <span className='item-indx'>{item.index}</span>
+                                    </div>
                                     <figure className="drag-catg-img">
-                                        <img src={item?.category_image} alt="img"   width= "55.381px" height={"55.381px"}/>
+                                        <img src={item?.category_image} alt="img" width="55.381px" height={"55.381px"} />
                                     </figure>
                                     <div className=""></div>
                                     <h3 className='itemCategory'>{item?.category}</h3>
-                                    
+
                                     <div className="button-arrow">
-                                    <img src={line}/>
-                                        <img src={arrow} height={"25px"} width={"25px"}/>
+                                        <img src={line} />
+                                        <img src={arrow} height={"25px"} width={"25px"} />
                                         {/* <svg
                                             xmlns="http://www.w3.org/2000/svg"
                                             width="5"
