ef731af38242266360dbb5d9d425bbf2f12dbde3
Astha Sharma <aniruddh.a@neosoftmail.com>
2023-11-27 14:16:57 +0530

    DND

--- src/Components/DashboardComponents/DndCategories/DndCategories.js
+++ src/Components/DashboardComponents/DndCategories/DndCategories.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import "../Categories/categories.css";
-import { UpdateMenuCategoryAfterDragAndDrop } from '../../../Redux/slices/menuSlice';
+import { UpdateMenuCategoryAfterDragAndDrop, GetMenuCategorySlice } from '../../../Redux/slices/menuSlice';
 import arrow from "../../../images/Arrow - Right 3.svg";
 import line from "../../../images/Vector 1.svg";
 
@@ -9,47 +9,52 @@ function DndCategories() {
     const dispatch = useDispatch();
     const MenuApiSelectorData = useSelector((state) => state.MenuApiData);
     const [draggedItem, setDraggedItem] = useState(null);
-    // const [items, setItems] = useState(MenuApiSelectorData?.GetMenuCategoryReducerData?.data);
-    const [items, setItems] = useState([
-    {
-        "menu_id": "b8712be2-6dd8-47ed-aa96-f5c17ae88e0d",
-        "category": "cake",
-        "category_image": null,
-        "index": 1
-    },
-    {
-        "menu_id": "11300c2d-3c66-47af-a3cc-467bbcf00f90",
-        "category": "drink",
-        "category_image": "https://restaurant-qr-staging.s3.ap-south-1.amazonaws.com/menu/diskette_Yr6ex9S.png",
-        "index": 2
-    },
-    {
-        "menu_id": "d17e8efb-7fa6-439b-a9d0-9ea2f7175cd0",
-        "category": "paneer Pizza",
-        "category_image": null,
-        "index": 3
-    },
-    
-    {
-        "menu_id": "d13a8cda-bc78-454d-815f-383773097c39",
-        "category": "burger",
-        "category_image": null,
-        "index": 4
-    },
-    {
-        "menu_id": "86bd1171-38c8-4311-99cd-7683c5903332",
-        "category": "Pizza",
-        "category_image": null,
-        "index": 5
-    },
-    {
-        "menu_id": "0f533415-196b-4baa-9d3b-ce3c568a61c6",
-        "category": "Paneer",
-        "category_image": null,
-        "index": 6
-    },
-  
-]);
+    const [dndPayload, setDndPayload] = useState()
+    const [items, setItems] = useState(MenuApiSelectorData?.GetMenuCategoryReducerData?.data);
+    const resId = localStorage.getItem("RestaurantId")
+
+
+    //     const [items, setItems] = useState(
+    //         [
+    //     {
+    //         "menu_id": "b8712be2-6dd8-47ed-aa96-f5c17ae88e0d",
+    //         "category": "cake",
+    //         "category_image": null,
+    //         "index": 1
+    //     },
+    //     {
+    //         "menu_id": "11300c2d-3c66-47af-a3cc-467bbcf00f90",
+    //         "category": "drink",
+    //         "category_image": "https://restaurant-qr-staging.s3.ap-south-1.amazonaws.com/menu/diskette_Yr6ex9S.png",
+    //         "index": 2
+    //     },
+    //     {
+    //         "menu_id": "d17e8efb-7fa6-439b-a9d0-9ea2f7175cd0",
+    //         "category": "paneer Pizza",
+    //         "category_image": null,
+    //         "index": 3
+    //     },
+
+    //     {
+    //         "menu_id": "d13a8cda-bc78-454d-815f-383773097c39",
+    //         "category": "burger",
+    //         "category_image": null,
+    //         "index": 4
+    //     },
+    //     {
+    //         "menu_id": "86bd1171-38c8-4311-99cd-7683c5903332",
+    //         "category": "Pizza",
+    //         "category_image": null,
+    //         "index": 5
+    //     },
+    //     {
+    //         "menu_id": "0f533415-196b-4baa-9d3b-ce3c568a61c6",
+    //         "category": "Paneer",
+    //         "category_image": null,
+    //         "index": 6
+    //     },
+
+    // ]);
     const [ActiveCategory, setActiveCategory] = useState(undefined);
     console.log("items", items)
 
@@ -90,50 +95,41 @@ function DndCategories() {
 
     const handleDrop = (e, item) => {
         e.preventDefault();
-    
+
         if (!draggedItem || draggedItem === item) {
             return;
         }
-    
-        // Find the index of the dragged and target items
+
         const draggedIndex = items.findIndex((el) => el.menu_id === draggedItem.menu_id);
         const targetIndex = items.findIndex((el) => el.menu_id === item.menu_id);
-    
+
         const updatedItems = [...items];
-        const newItems = [];
-    
-        // Iterate over the items and reindex them
-        updatedItems.forEach((originalItem, index) => {
-            console.log("originalItem",originalItem)
-            if (index === targetIndex) {
-                // Insert the dragged item at the target position
-                console.log("index1",index,targetIndex)
-                newItems.push({ ...draggedItem, index: index + 1 });
-            }
-            // Skip the dragged item if it's not at the target position
-            if (index !== draggedIndex) {
-                console.log("index2",index,draggedIndex)
-                const newIndex = index >= targetIndex ? index + 2 : index;
-                newItems.push({ ...originalItem, index: newIndex });
-            }
-        });
-    
-        // Extract only menu_id and index properties for the payload
+        const draggedItemCopy = { ...draggedItem, index: targetIndex + 1 };
+
+        // Remove the dragged item from its original position
+        updatedItems.splice(draggedIndex, 1);
+
+        // Insert the dragged item at the target position
+        updatedItems.splice(targetIndex, 0, draggedItemCopy);
+
+        // Reindex the remaining items
+        const newItems = updatedItems.map((item, index) => ({ ...item, index: index + 1 }));
+
+        setItems(newItems);
+        console.log("newItems", newItems);
+
         const payload = {
             data: newItems.map(({ menu_id, index }) => ({ menu_id, index })),
         };
-    
-        setItems(newItems);
-        console.log("newItems", newItems);
         console.log("payload", payload);
-    
+        setDndPayload(payload)
         // Dispatch an action to update the Redux store with the new order
-        dispatch(UpdateMenuCategoryAfterDragAndDrop(payload));
-    
+        // dispatch(UpdateMenuCategoryAfterDragAndDrop(payload));
+        // dispatch(GetMenuCategorySlice());
         setDraggedItem(null);
     };
-    
- 
+
+
     // const handleDrop = (e, item) => {
     //     e.preventDefault();
 
@@ -160,17 +156,39 @@ function DndCategories() {
     //     setDraggedItem(null);
     // };
 
-
-
-
-
     const handleDragEnd = () => {
         setDraggedItem(null);
     };
 
+    const handleDndUpdate = () => {
+        dispatch(UpdateMenuCategoryAfterDragAndDrop(dndPayload));
+
+    };
+    useEffect(() => {
+        console.log("fbdfbfghjghh1");
+        if (MenuApiSelectorData?.GetMenuCategoryReducerData?.status === 200) {
+            console.log("fbdfbfghjghh");
+
+            // Additional actions you want to perform when status is 200
+            dispatch(GetMenuCategorySlice({ RestaurantId: resId }));
+        } else {
+            console.log("Error or status not 200.");
+        }
+    }, [MenuApiSelectorData?.UpdateMenuCategoryAfterDragAndDroprReducerData])
+
     return (
         <div style={{ width: "100%", height: "100vh", display: "flex", flexDirection: "column", backgroundColor: "FBF6F2", padding: "30px" }}>
-            <h1 className='recorded-cat-head'>Reorder Categories</h1>
+            <div className="recorded-cat-div">
+                <h1 className='recorded-cat-head'>Reorder Categories</h1>
+                <button
+                    type="button"
+                    className="categorybtn btn2"
+                    onClick={handleDndUpdate}
+                    style={{ width: "100px",height:"43px" }}
+                >
+                    Save
+                </button>
+            </div>
             <div
             // className="categorycontent"
             >
