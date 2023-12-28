dfa247bfc939c3055bbe5bc0781d003ca3fff551
Om Veer Singh <omveersingh@externlabs.com>
2023-12-28 15:31:42 +0530

    Changed design according to figma.

--- src/Components/DashboardComponents/DashboardSidebar/DashboardSidebar.js
+++ src/Components/DashboardComponents/DashboardSidebar/DashboardSidebar.js
@@ -3,14 +3,18 @@ import './dashboardSidebar.css'
 import logo from '../../../images/logo2.svg'
 import fulllogo from '../../../images/logo.svg'
 import icon7 from '../../../images/icon7.svg'
+import icon7h from '../../../images/icon7h.svg'
 import icon8 from '../../../images/icon8.svg'
 import icon8h from '../../../images/icon8h.svg'
 import icon9 from '../../../images/icon9.svg'
 import icon10 from '../../../images/icon10.svg'
+import icon10h from '../../../images/icon10h.svg'
 import icon11 from '../../../images/icon11.svg'
 
 import icon13 from '../../../images/icon13.svg'
+import icon13h from '../../../images/icon13h.svg'
 import icon14 from '../../../images/icon14.svg'
+import icon14h from '../../../images/icon14h.svg'
 import { ToggleBar } from '../../../Redux/slices/sideBarToggle'
 import { useDispatch, useSelector } from 'react-redux'
 import { useLocation, NavLink, useNavigate, Route } from "react-router-dom";
@@ -20,12 +24,12 @@ import { useTranslation } from 'react-i18next';
 const DashboardSidebar = () => {
 
 
-  const {t, i18n } = useTranslation()
+  const { t, i18n } = useTranslation()
 
   let UserTypeData = reactLocalStorage.get("Type", false);
 
-  let  RestaurantId= reactLocalStorage.get("RestaurantId",false);
- 
+  let RestaurantId = reactLocalStorage.get("RestaurantId", false);
+
 
 
   const [ToggleHeader, setToggleHeader] = useState()
@@ -47,9 +51,9 @@ const DashboardSidebar = () => {
   }, [ToggleHeader])
 
 
-  useEffect(()=>{
+  useEffect(() => {
     i18n.changeLanguage(ManagerApiSelectorData?.languagechange)
-  },[ManagerApiSelectorData?.languagechange])
+  }, [ManagerApiSelectorData?.languagechange])
 
 
   return (
@@ -61,56 +65,81 @@ const DashboardSidebar = () => {
         </figure>
         <ul className='navmenu'>
           {UserTypeData !== "sales"
-          &&
+            &&
             <>
 
               <li className="">
                 <NavLink
                   to={`/${RestaurantId}/admin/dashboard`}
                   className={` ${param.pathname === ":id/admin/dashboard" ? "active" : ""
-                    } `}
-                >
-                  <img src={icon7} alt='img' /> <span className='text'>{t("dashboard")}</span>
-               
+                    } `} >
+                  <div className='normalicon'>
+                    <img src={icon7} alt='img' /> <span className='text'>{t("dashboard")}</span>
+                  </div>
+
+                  <div className='activeicon'>
+                    <img src={icon7h} alt='icon8h img' /> <span className='text'>{t("dashboard")}</span>
+                  </div>
                 </NavLink>
               </li>
+
               <li>
                 <NavLink to={`/${RestaurantId}/admin/manager`}
                   className={` ${param.pathname === ":id/admin/manager" ? "active" : ""
                     } `}
-                >   
-                
-                <div className='normalicon'>
-                <img src={icon8} alt='img' /> <span className='text'>{t("managers")}</span>
-                </div>
+                >
+                  <div className='normalicon'>
+                    <img src={icon8} alt='img' /> <span className='text'>{t("managers")}</span>
+                  </div>
 
-                <div className='activeicon'>
-                  <img src={icon8h} alt='icon8h img' /> <span className='text'>{t("managers")}</span>
-                </div>
-
-
-
+                  <div className='activeicon'>
+                    <img src={icon8h} alt='icon8h img' /> <span className='text'>{t("managers")}</span>
+                  </div>
 
                 </NavLink>
               </li>
+
               <li>
                 <NavLink to={`/${RestaurantId}/admin/categories`} className={` ${param.pathname === ":id/admin/categories" ? "active" : ""
-                  } `}> <img src={icon10} alt='img' /> <span className='text'>{t("menu-categories")}</span>
+                  } `}>
+                  <div className='normalicon'>
+                    <img src={icon10} alt='img' /> <span className='text'>{t("menu-categories")}</span>
+                  </div>
+
+                  <div className='activeicon'>
+                    <img src={icon10h} alt='icon8h img' /> <span className='text'>{t("menu-categories")}</span>
+                  </div>
                 </NavLink>
               </li>
 
               <li>
                 <NavLink to={`/${RestaurantId}/admin/allmedia`} className={` ${param.pathname === ":id/admin/allmedia" ? "active" : ""
-                  } `}> <img src={icon13} alt='img' /> <span className='text'> {t("media-files")} </span>
+                  } `}>
+
+                  <div className='normalicon'>
+                    <img src={icon13} alt='img' /> <span className='text'>{t("media-files")}</span>
+                  </div>
+
+                  <div className='activeicon'>
+                    <img src={icon13h} alt='icon8h img' /> <span className='text'>{t("media-files")}</span>
+                  </div>
+
                 </NavLink>
               </li>
 
               <li>
                 <NavLink to={`/${RestaurantId}/admin/paymenthistory`} className={` ${param.pathname === ":id/admin/paymenthistory" ? "active" : ""
-                  } `}> <img src={icon14} alt='img' /> <span className='text'>{t("payment-history")}</span>
+                  } `}>
+
+                  <div className='normalicon'>
+                    <img src={icon14} alt='img' /> <span className='text'>{t("payment-history")}</span>
+                  </div>
+
+                  <div className='activeicon'>
+                    <img src={icon14h} alt='icon8h img' /> <span className='text'>{t("payment-history")}</span>
+                  </div>
                 </NavLink>
               </li>
-
 
               {/* <li>
                 <NavLink
@@ -127,31 +156,39 @@ const DashboardSidebar = () => {
           {UserTypeData === "sales" && <>  <li>
             <NavLink
               to={`/admin/leads`}
-              className={` ${param.pathname === "/admin/leads" || param?.state?.currentData?.lead_id  ? "active" : ""
+              className={` ${param.pathname === "/admin/leads" || param?.state?.currentData?.lead_id ? "active" : ""
                 } `}
 
             >
-             <div className='normalicon'>
+              <div className='normalicon'>
                 <img src={icon8} alt='img' /> <span className='text'>{t("leads")}</span>
-                </div>
+              </div>
 
-                <div className='activeicon'>
-                  <img src={icon8h} alt='icon8h img' /> <span className='text'>{t("leads")}</span>
-                </div>
-            
-            {/* <img src={icon8} alt='img' /> <span className='text'>Leads</span> */}
+              <div className='activeicon'>
+                <img src={icon8h} alt='icon8h img' /> <span className='text'>{t("leads")}</span>
+              </div>
+
+              {/* <img src={icon8} alt='img' /> <span className='text'>Leads</span> */}
 
             </NavLink>
           </li>
             <li>
               <NavLink
-                to=  {`/admin/restaurant`}  
+                to={`/admin/restaurant`}
                 className={` ${param.pathname === "/admin/restaurant" || param?.state?.currentData?.restaurant_id ? "active" : ""
                   } `}
 
-            ><img src={icon10} alt='img' /> <span className='text'>{t("restaurants")}</span>
-            </NavLink>
-          </li>
+              >
+                <div className='normalicon'>
+                  <img src={icon10} alt='img' /> <span className='text'>{t("restaurants")}</span>
+                </div>
+
+                <div className='activeicon'>
+                  <img src={icon10h} alt='icon8h img' /> <span className='text'>{t("restaurants")}</span>
+                </div>
+
+              </NavLink>
+            </li>
           </>}
           {/* <li>
             <NavLink
