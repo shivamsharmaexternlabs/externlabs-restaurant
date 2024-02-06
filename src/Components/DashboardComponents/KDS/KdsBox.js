bcd97d27bfc02b427e73c1e2987616cc04723d6d
Astha Sharma <aniruddh.a@neosoftmail.com>
2024-02-06 18:55:57 +0530

    updatd kds code

--- src/Components/DashboardComponents/KDS/KdsBox.js
+++ src/Components/DashboardComponents/KDS/KdsBox.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './kdsScreen.css'
 import foodparcel from '../../../images/food-parcel.svg'
 import cooking from '../../../images/cooking.svg'
@@ -6,370 +6,214 @@ import DineIn from '../../../images/plate.svg'
 import takeAwayVerified from '../../../images/take-away-verified.svg'
 import DineInVerified from '../../../images/dineInVerified.svg'
 
+
 function KdsBox() {
+
+  const [data, setdata] = useState([
+    {
+      id: 1,
+      name: "Table",
+      tableNo: 1,
+      orderType: "take Away",
+      orderNo: "#2309",
+      orderDetails: [
+        {
+          name: "Sandwich",
+          quantity: 1,
+          extras: "Extra cheese",
+          status: "cooking",
+        },
+        {
+          name: " Vegetable Pizza",
+          quantity: 2,
+          extras: "Extra cheese",
+          status: "cooking",
+        },
+        {
+          name: "Mocktails",
+          quantity: 2,
+          extras: "",
+          status: "done",
+        },
+      ],
+    },
+    {
+      id: 2,
+      name: "Table",
+      tableNo: 2,
+      orderType: "Dine In",
+      orderNo: "#2309",
+      orderDetails: [
+        {
+          name: "Sandwich",
+          quantity: 1,
+          extras: "Extra cheese",
+          status: "cooking",
+        },
+        {
+          name: " Vegetable Pizza",
+          quantity: 2,
+          extras: "Extra cheese",
+          status: "cooking",
+        },
+        {
+          name: "Mocktails",
+          quantity: 2,
+          extras: "",
+          status: "done",
+        },
+      ],
+    },
+    {
+      id: 3,
+      name: "Table",
+      tableNo: 3,
+      orderType: "take Away",
+      orderNo: "#2309",
+      orderDetails: [
+        {
+          name: "Sandwich",
+          quantity: 1,
+          extras: "Extra cheese",
+          status: "done",
+        },
+        {
+          name: " Vegetable Pizza",
+          quantity: 2,
+          extras: "Extra cheese",
+          status: "cooking",
+        },
+        {
+          name: "Mocktails",
+          quantity: 2,
+          extras: "",
+          status: "done",
+        },
+      ],
+    },
+    {
+      id: 4,
+      name: "Table",
+      tableNo: 4,
+      orderType: "Dine In",
+      orderNo: "#2309",
+      orderDetails: [
+        {
+          name: "Sandwich",
+          quantity: 1,
+          extras: "Extra cheese",
+          status: "cooking",
+        },
+        {
+          name: " Vegetable Pizza",
+          quantity: 2,
+          extras: "Extra cheese",
+          status: "cooking",
+        },
+        {
+          name: "Mocktails",
+          quantity: 2,
+          extras: "",
+          status: "done",
+        },
+      ],
+    },
+    {
+      id: 5,
+      name: "Table",
+      tableNo: 5,
+      orderType: "take Away",
+      orderNo: "#2309",
+      orderDetails: [
+        {
+          name: "Sandwich",
+          quantity: 1,
+          extras: "Extra cheese",
+          status: "cooking",
+        },
+        {
+          name: " Vegetable Pizza",
+          quantity: 2,
+          extras: "Extra cheese",
+          status: "cooking",
+        },
+        {
+          name: "Mocktails",
+          quantity: 7,
+          extras: "",
+          status: "cooking",
+        },
+      ],
+    },
+  ]);
+
   return (
     <div>
       <div className='kgcardpart'>
-        <div className='kgcardbox'>
-          <div className='kgcardtitle'>
-            <div className='leftpart'>
-              <h3>Name</h3>
-              <h4>00:10:53 </h4>
+        {data.map((item) => (
+          <div className='kgcardbox'>
+            <div className='kgcardtitle'
+              style={{
+                backgroundColor:
+                  item.orderType === "take Away" ? "#eabb36" : "#42B856",
+              }}>
+              <div className='leftpart'>
+                <h3>{item.name}</h3>
+                {/* <h4>00:10:53 </h4> */}
+              </div>
+              <div className='rightpart'>
+                <h2> Table No.{item.tableNo}</h2>
+              </div>
             </div>
-            <div className='rightpart'>
-              <h2> Table No.</h2>
-            </div>
+            {/* <div className='deliveryinfo'>
+                <h3> <img src={item.orderType == "take Away" ? foodparcel : DineIn} alt='img' /> {item.orderType}</h3>
+                <h4>{item.orderNo}</h4>
+              </div> */}
+            <ul className='kgcardlist' key={item.id}>
+              {item.orderDetails.map((order, index) => {
+                return (
+                  <>
+                    <li key={index}>
+                      <div className='leftpart'>
+                        <h3> {order.quantity} X {order.name}</h3>
+                        <p>{order.extras}</p>
+                      </div>
+                      <div className='rightpart'>
+                        <button
+                          type="button"
+                          style={{
+                            borderColor:
+                              item.orderType === "take Away"
+                                ? "#eabb36"
+                                : "#42B856",
+                            color:
+                              item.orderType === "take Away"
+                                ? "#eabb36"
+                                : "#42B856",
+                          }}
+                        >
+                          {order.status === "done"
+                            ? "Mark as done"
+                            : "Start Cooking"}{" "}
+                          <img
+                            src={
+                              order.status === "done"
+                                ? item.orderType === "take Away"
+                                  ? takeAwayVerified
+                                  : DineInVerified
+                                : cooking
+                            }
+                            alt="img"
+                          />
+                        </button>
+                      </div>
+                    </li>
+                  </>
+                )
+              })}
+            </ul>
           </div>
-          <div className='deliveryinfo'>
-            <h3> <img src={foodparcel} alt='img' /> Take Away</h3>
-            <h4>#2309</h4>
-          </div>
-
-          <ul className='kgcardlist'>
-            <li>
-              <div className='leftpart'>
-                <h3>1 X Sandwich</h3>
-                <p>Extra cheese</p>
-              </div>
-              <div className='rightpart'>
-                <button type='button'> Start Cooking  <img src={cooking} alt='img' /> </button>
-              </div>
-            </li>
-            <li>
-              <div className='leftpart'>
-                <h3>1 X Sandwich</h3>
-                <p>Extra cheese</p>
-              </div>
-              <div className='rightpart'>
-                <button type='button'> Mark as Done<img src={takeAwayVerified} alt='img' /> </button>
-              </div>
-            </li>
-            <li>
-              <div className='leftpart'>
-                <h3>1 X Sandwich</h3>
-                <p>Extra cheese</p>
-              </div>
-              <div className='rightpart'>
-                <button type='button'> Start Cooking  <img src={cooking} alt='img' /> </button>
-              </div>
-            </li>            
-          </ul>
-        </div>
-        <div className='kgcardbox'>
-          <div className='kgcardtitle' style={{ backgroundColor: "#42B856" }}>
-            <div className='leftpart'>
-              <h3>Name</h3>
-              <h4>00:10:53 </h4>
-            </div>
-            <div className='rightpart'>
-              <h2> Table No.</h2>
-            </div>
-          </div>
-          <div className='deliveryinfo'>
-            <h3> <img src={DineIn} alt='img' /> Dine In</h3>
-            <h4>#2309</h4>
-          </div>
-
-          <ul className='kgcardlist' >
-            <li >
-              <div className='leftpart'>
-                <h3>1 X Sandwich</h3>
-                <p>Extra cheese</p>
-              </div>
-              <div className='rightpart'>
-                <button type='button' style={{ border: "1px solid #42B856", color: "#42B856" }}> Start Cooking  <img src={cooking} alt='img' /> </button>
-              </div>
-            </li>
-            <li>
-              <div className='leftpart'>
-                <h3>1 X Sandwich</h3>
-                <p>Extra cheese</p>
-              </div>
-              <div className='rightpart'>
-                <button type='button' style={{ border: "1px solid #42B856", color: "#42B856" }}> Mark as Done <img src={DineInVerified} alt='img' /> </button>
-              </div>
-            </li>
-            <li>
-              <div className='leftpart'>
-                <h3>1 X Sandwich</h3>
-                <p>Extra cheese</p>
-              </div>
-              <div className='rightpart'>
-                <button type='button' style={{ border: "1px solid #42B856", color: "#42B856" }}> Start Cooking  <img src={cooking} alt='img' /> </button>
-              </div>
-            </li>
-          </ul>
-        </div>
-        <div className='kgcardbox'>
-          <div className='kgcardtitle'>
-            <div className='leftpart'>
-              <h3>Name</h3>
-              <h4>00:10:53 </h4>
-            </div>
-            <div className='rightpart'>
-              <h2> Table No.</h2>
-            </div>
-          </div>
-          <div className='deliveryinfo'>
-            <h3> <img src={foodparcel} alt='img' /> Take Away</h3>
-            <h4>#2309</h4>
-          </div>
-
-          <ul className='kgcardlist'>
-            <li>
-              <div className='leftpart'>
-                <h3>1 X Sandwich</h3>
-                <p>Extra cheese</p>
-              </div>
-              <div className='rightpart'>
-                <button type='button'> Start Cooking  <img src={cooking} alt='img' /> </button>
-              </div>
-            </li>
-            <li>
-              <div className='leftpart'>
-                <h3>1 X Sandwich</h3>
-                <p>Extra cheese</p>
-              </div>
-              <div className='rightpart'>
-                <button type='button'> Start Cooking  <img src={cooking} alt='img' /> </button>
-              </div>
-            </li>
-            <li>
-              <div className='leftpart'>
-                <h3>1 X Sandwich</h3>
-                <p>Extra cheese</p>
-              </div>
-              <div className='rightpart'>
-                <button type='button'> Start Cooking  <img src={cooking} alt='img' /> </button>
-              </div>
-            </li>
-          </ul>
-        </div>
-        <div className='kgcardbox'>
-          <div className='kgcardtitle'>
-            <div className='leftpart'>
-              <h3>Name</h3>
-              <h4>00:10:53 </h4>
-            </div>
-            <div className='rightpart'>
-              <h2> Table No.</h2>
-            </div>
-          </div>
-          <div className='deliveryinfo'>
-            <h3> <img src={foodparcel} alt='img' /> Take Away</h3>
-            <h4>#2309</h4>
-          </div>
-
-          <ul className='kgcardlist'>
-            <li>
-              <div className='leftpart'>
-                <h3>1 X Sandwich</h3>
-                <p>Extra cheese</p>
-              </div>
-              <div className='rightpart'>
-                <button type='button'> Start Cooking  <img src={cooking} alt='img' /> </button>
-              </div>
-            </li>
-            <li>
-              <div className='leftpart'>
-                <h3>1 X Sandwich</h3>
-                <p>Extra cheese</p>
-              </div>
-              <div className='rightpart'>
-                <button type='button'> Start Cooking  <img src={cooking} alt='img' /> </button>
-              </div>
-            </li>
-            <li>
-              <div className='leftpart'>
-                <h3>1 X Sandwich</h3>
-                <p>Extra cheese</p>
-              </div>
-              <div className='rightpart'>
-                <button type='button'> Start Cooking  <img src={cooking} alt='img' /> </button>
-              </div>
-            </li>
-          </ul>
-        </div>
-        <div className='kgcardbox'>
-          <div className='kgcardtitle'>
-            <div className='leftpart'>
-              <h3>Name</h3>
-              <h4>00:10:53 </h4>
-            </div>
-            <div className='rightpart'>
-              <h2> Table No.</h2>
-            </div>
-          </div>
-          <div className='deliveryinfo'>
-            <h3> <img src={foodparcel} alt='img' /> Take Away</h3>
-            <h4>#2309</h4>
-          </div>
-
-          <ul className='kgcardlist'>
-            <li>
-              <div className='leftpart'>
-                <h3>1 X Sandwich</h3>
-                <p>Extra cheese</p>
-              </div>
-              <div className='rightpart'>
-                <button type='button'> Start Cooking  <img src={cooking} alt='img' /> </button>
-              </div>
-            </li>
-            <li>
-              <div className='leftpart'>
-                <h3>1 X Sandwich</h3>
-                <p>Extra cheese</p>
-              </div>
-              <div className='rightpart'>
-                <button type='button'> Start Cooking  <img src={cooking} alt='img' /> </button>
-              </div>
-            </li>
-            <li>
-              <div className='leftpart'>
-                <h3>1 X Sandwich</h3>
-                <p>Extra cheese</p>
-              </div>
-              <div className='rightpart'>
-                <button type='button'> Start Cooking  <img src={cooking} alt='img' /> </button>
-              </div>
-            </li>
-          </ul>
-        </div>
-        <div className='kgcardbox'>
-          <div className='kgcardtitle'>
-            <div className='leftpart'>
-              <h3>Name</h3>
-              <h4>00:10:53 </h4>
-            </div>
-            <div className='rightpart'>
-              <h2> Table No.</h2>
-            </div>
-          </div>
-          <div className='deliveryinfo'>
-            <h3> <img src={foodparcel} alt='img' /> Take Away</h3>
-            <h4>#2309</h4>
-          </div>
-
-          <ul className='kgcardlist'>
-            <li>
-              <div className='leftpart'>
-                <h3>1 X Sandwich</h3>
-                <p>Extra cheese</p>
-              </div>
-              <div className='rightpart'>
-                <button type='button'> Start Cooking  <img src={cooking} alt='img' /> </button>
-              </div>
-            </li>
-            <li>
-              <div className='leftpart'>
-                <h3>1 X Sandwich</h3>
-                <p>Extra cheese</p>
-              </div>
-              <div className='rightpart'>
-                <button type='button'> Start Cooking  <img src={cooking} alt='img' /> </button>
-              </div>
-            </li>
-            <li>
-              <div className='leftpart'>
-                <h3>1 X Sandwich</h3>
-                <p>Extra cheese</p>
-              </div>
-              <div className='rightpart'>
-                <button type='button'> Start Cooking  <img src={cooking} alt='img' /> </button>
-              </div>
-            </li>
-          </ul>
-        </div>
-        <div className='kgcardbox'>
-          <div className='kgcardtitle'>
-            <div className='leftpart'>
-              <h3>Name</h3>
-              <h4>00:10:53 </h4>
-            </div>
-            <div className='rightpart'>
-              <h2> Table No.</h2>
-            </div>
-          </div>
-          <div className='deliveryinfo'>
-            <h3> <img src={foodparcel} alt='img' /> Take Away</h3>
-            <h4>#2309</h4>
-          </div>
-
-          <ul className='kgcardlist'>
-            <li>
-              <div className='leftpart'>
-                <h3>1 X Sandwich</h3>
-                <p>Extra cheese</p>
-              </div>
-              <div className='rightpart'>
-                <button type='button'> Start Cooking  <img src={cooking} alt='img' /> </button>
-              </div>
-            </li>
-            <li>
-              <div className='leftpart'>
-                <h3>1 X Sandwich</h3>
-                <p>Extra cheese</p>
-              </div>
-              <div className='rightpart'>
-                <button type='button'> Start Cooking  <img src={cooking} alt='img' /> </button>
-              </div>
-            </li>
-            <li>
-              <div className='leftpart'>
-                <h3>1 X Sandwich</h3>
-                <p>Extra cheese</p>
-              </div>
-              <div className='rightpart'>
-                <button type='button'> Start Cooking  <img src={cooking} alt='img' /> </button>
-              </div>
-            </li>
-          </ul>
-        </div>
-        <div className='kgcardbox'>
-          <div className='kgcardtitle'>
-            <div className='leftpart'>
-              <h3>Name</h3>
-              <h4>00:10:53 </h4>
-            </div>
-            <div className='rightpart'>
-              <h2> Table No.</h2>
-            </div>
-          </div>
-          <div className='deliveryinfo'>
-            <h3> <img src={foodparcel} alt='img' /> Take Away</h3>
-            <h4>#2309</h4>
-          </div>
-
-          <ul className='kgcardlist'>
-            <li>
-              <div className='leftpart'>
-                <h3>1 X Sandwich</h3>
-                <p>Extra cheese</p>
-              </div>
-              <div className='rightpart'>
-                <button type='button'> Start Cooking  <img src={cooking} alt='img' /> </button>
-              </div>
-            </li>
-            <li>
-              <div className='leftpart'>
-                <h3>1 X Sandwich</h3>
-                <p>Extra cheese</p>
-              </div>
-              <div className='rightpart'>
-                <button type='button'> Start Cooking  <img src={cooking} alt='img' /> </button>
-              </div>
-            </li>
-            <li>
-              <div className='leftpart'>
-                <h3>1 X Sandwich</h3>
-                <p>Extra cheese</p>
-              </div>
-              <div className='rightpart'>
-                <button type='button'> Start Cooking  <img src={cooking} alt='img' /> </button>
-              </div>
-            </li>
-          </ul>
-        </div>
+        ))}
       </div>
     </div>
   )
--- src/Components/DashboardComponents/KDS/KdsScreen.js
+++ src/Components/DashboardComponents/KDS/KdsScreen.js
@@ -1,26 +1,30 @@
 import React from 'react'
 import kdslogo from '../../../images/logo2.svg'
 import './kdsScreen.css'
-import KdsNotification from '../../../images/KdsNotification.svg'
-// import foodparcel from '../../../images/food-parcel.svg'
-// import cooking from '../../../images/cooking.svg'
-// import DineIn from '../../../images/plate.svg'
-// import takeAwayVerified from '../../../images/take-away-verified.svg'
-// import DineInVerified from '../../../images/dineInVerified.svg'
+// import KdsNotification from '../../../images/KdsNotification.svg'
+import Logout from '../../../images/logout.svg'
 import KdsBox from './KdsBox'
 
 
 function KdsScreen() {
+
+    const handleLogout = () => {
+        alert("logout successfull")
+    }
+
     return (
         <>
             <div className='kdspage'>
                 <div className='kds-header'>
                     <img className='kdslogoimg' alt='logo_img' src={kdslogo} />
-                    <img className="kdsnotifimg" alt="Notification_Icon" src={KdsNotification} />
+                    <button className='kds-logout' onClick={handleLogout}>
+                        <img className="kds-logoutimg" alt="Notification_Icon" src={Logout} />
+                        <span>Log Out</span>
+                    </button>
                 </div>
-
+                
                 <div className='contentpart'>
-                    <KdsBox/>                   
+                    <KdsBox />
                 </div>
             </div>
         </>
