7e93169a4605caedc262c7a8af6e56fe74f3ee49
Astha Sharma <aniruddh.a@neosoftmail.com>
2024-03-15 17:44:48 +0530

    api getwaiternames

--- src/Redux/slices/KotOrdersHistorySlice.js
+++ src/Redux/slices/KotOrdersHistorySlice.js
@@ -1,13 +1,57 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
+
 export const GetOrdersHistorySlice = createAsyncThunk(
-    "GetOrdersHistorySlice",
+  "GetOrdersHistorySlice",
+  async (body, { rejectWithValue }) => {
+      try {
+          // Construct the base URL
+          let url = `${process.env.REACT_APP_BASE_URL}kds/order_history/?restaurant_id=${body.restaurant_id}`;
+
+          // Check if start_date is provided and not undefined
+          if (body.start_date !== undefined) {
+              // Append the start_date parameter to the URL
+              url += `&start_date=${body.start_date}`;
+          }
+
+          // Check if end_date is provided and not undefined
+          if (body.end_date !== undefined) {
+              // Append the end_date parameter to the URL
+              url += `&end_date=${body.end_date}`;
+          }
+
+          // Check if waiter is provided and not undefined
+          if (body.waiter !== undefined) {
+              // Append the waiter parameter to the URL
+              url += `&waiter=${body.waiter}`;
+          }
+          if (body.page !== undefined) {
+            // Append the page parameter to the URL
+            url += `&page=${body.page}`;
+        }
+
+          const response = await axios.get(url, {
+              headers: {
+                  Authorization: `Bearer ${body.token}`                
+              }
+          });
+
+          return response;
+      } catch (err) {
+          return rejectWithValue(err);
+      }
+  }
+);
+
+
+
+  export const GetWaiterNameSlice = createAsyncThunk(
+    "GetWaiterNameSlice",
     async (body, { rejectWithValue }) => {
-      // kot/?restaurant_id=89c2cce5-c2c2-4159-9bb6-ff4dcfa17e82&table_id=51cd9a2d-83c6-4598-bbae-45b445962911'
        try {
         const response = await axios.get(
-          `${process.env.REACT_APP_BASE_URL}kds/kot/?restaurant_id=${body.restaurant_id}&table_id=${body?.tableId?body?.tableId:""}`,
+          `${process.env.REACT_APP_BASE_URL}restaurant_app/staff/?restaurant_id=${body.restaurant_id}`,
           {
             headers: {
                 Authorization: `Bearer ${body.token}`                
@@ -22,13 +66,13 @@ export const GetOrdersHistorySlice = createAsyncThunk(
       }
     }
   );
-
   
 
   export const OrdersHistoryReducer = createSlice({
     name: "OrdersHistoryReducer",
     initialState: {
       GetOrdersHistoryData: [],
+      GetWaiterData:[],
       loading: false,
       error: null,
     },
@@ -46,6 +90,19 @@ export const GetOrdersHistorySlice = createAsyncThunk(
       })
 
       .addCase(GetOrdersHistorySlice.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+      })
+      .addCase(GetWaiterNameSlice.pending, (state) => {
+        state.loading = true;
+      })
+
+      .addCase(GetWaiterNameSlice.fulfilled, (state, action) => {
+        state.loading = false;
+        state.GetWaiterData = action.payload;
+      })
+
+      .addCase(GetWaiterNameSlice.rejected, (state, action) => {
         state.loading = false;
         state.error = action.error.message;
       })
