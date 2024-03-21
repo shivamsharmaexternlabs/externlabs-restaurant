b4900dcb1280fef48c4d6aecdfa6dfde0b17e2f7
Astha Sharma <aniruddh.a@neosoftmail.com>
2024-03-21 12:00:54 +0530

    replace more text to view

--- src/Components/DashboardComponents/KotOrderHistory/OrderHistoryBox.js
+++ src/Components/DashboardComponents/KotOrderHistory/OrderHistoryBox.js
@@ -33,16 +33,6 @@ function OrderHistoryBox({ translaterFun }) {
     const token = localStorage.getItem('Token')
     const [startDate, setStartDate] = useState("");
     const [endDate, setEndDate] = useState("");
-    const [translaterText, setTranslaterText] = useState("more");
-
-    const translaterFunction = (text) => {
-        if (text === "more") {
-            setTranslaterText("less");
-        } else {
-            setTranslaterText("more");
-        }
-    };
-
 
     const handleDateRangeChange = (dateRange) => {
         setSelectedDateRange(dateRange);
@@ -281,9 +271,7 @@ function OrderHistoryBox({ translaterFun }) {
                                                     {kotItem?.item?.item_name_en?.length > 25 ?
                                                         <>
                                                             {kotItem?.item?.item_name_en?.slice(0, 30) + "..."}
-                                                            <span className='more-text' onMouseEnter={() => translaterFunction("more")} onMouseLeave={() => translaterFunction("less")}>
-                                                                <b>{translaterFun(translaterText)}</b>
-                                                            </span>
+                                                            <span className='more-text'><b>{translaterFun("view")}</b></span>
 
                                                             <div className="full-text">{kotItem?.item?.item_name_en}</div>
                                                         </> :
@@ -295,9 +283,7 @@ function OrderHistoryBox({ translaterFun }) {
                                                     {kotItem?.item?.item_name_native?.length > 25 ?
                                                         <>
                                                             {kotItem?.item?.item_name_native?.slice(0, 30) + "..."}
-                                                            <span className='more-text' onMouseEnter={() => translaterFunction("more")} onMouseLeave={() => translaterFunction("less")}>
-                                                                <b>{translaterFun(translaterText)}</b>
-                                                            </span>
+                                                            <span className='more-text'><b>{translaterFun("view")}</b></span>
                                                             <div className="full-text">{kotItem?.item?.item_name_native}</div>
                                                         </> :
                                                         kotItem?.item?.item_name_native
